eb8aaac116e9cd95f91d22f581b3606c11d52549
Leon Gabriel <g73346920@gmail.com>
2025-02-12 11:22:50 +0300

    Chored:App wrapped with react-native-safe-area-context to prevennt UI overlap with devices notches.

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,15 +1,22 @@
 import { Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+<SafeAreaView style={{ flex: 1}}>
+  {/* Other Components go here */}
+</SafeAreaView>
+
+    </SafeAreaProvider>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Text>Edit app/index.tsx to edit this screen.</Text>
+    // </View>
   );
 }
