8f60b349c35f4e5713e898c955ecc44d1c0e8a60
Leon Gabriel <g73346920@gmail.com>
2025-02-14 18:43:11 +0300

    Chored:headerShown set to false inorder to hide the default header that the react navigation .(which expo-router is build on) provides

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack screenOptions={{
+    headerShown: false  // hides the default header thaat the react navigation(which expo-router is built on) provides.
+  }} />;
 }
